3fe6027292a064d2d2041383b41d278d8ab674ca
hamzakhalem <hamzakhalem97@gmail.com>
2024-06-11 22:25:57 +0100

    fix: logout issue

--- portfolioapp/urls.py
+++ portfolioapp/urls.py
@@ -4,7 +4,7 @@ from .views import home
 from django.conf.urls.i18n import i18n_patterns
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('i18n', include('django.conf.urls.i18n')),
+    # path('i18n', include('django.conf.urls.i18n')),
     path('en/', home, name="index"),
     path('fr/', home, name="index"),
     path('ar/', home, name="index"),
