fe2dab605038ecc5bd470519a3320be7a1f76ffc
unknown <hamzakhalem97@gmail.com>
2025-02-07 21:13:38 +0100

    langs

--- portfolioapp/urls.py
+++ portfolioapp/urls.py
@@ -1,12 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home
+from .views import home, homefr, homear
 from django.conf.urls.i18n import i18n_patterns
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    # path('i18n', include('django.conf.urls.i18n')),
+    path('/', home, name="index"),
     path('en/', home, name="index"),
-    path('fr/', home, name="index"),
-    path('ar/', home, name="index"),
+    path('fr/', homefr, name="indexfr"),
+    path('ar/', homear, name="indexar"),
     
 ]
--- portfolioapp/views.py
+++ portfolioapp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def  home(request):
     return render (request, 'index.html')
+def  homefr(request):
+    return render (request, 'indexfr.html')
+def  homear(request):
+    return render (request, 'indexar.html')
